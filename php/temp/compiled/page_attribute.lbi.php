62960ee6c2d6282daff2c3805918151936e32023
Matthew <matthewxu@live.cn>
2011-05-23 09:34:35 +0800

    search page

--- php/temp/compiled/page_attribute.lbi.php
+++ php/temp/compiled/page_attribute.lbi.php
@@ -1,56 +1,43 @@
 <div id="lhs-ref">
-                <div id="ms">
-                    <ul>
-                        <li>
-                            <div>
-                                <span class="mode-icon"></span>
-                                <span class="mode-desc"><a href="">{allresult}所有结果</a></span>
-                            </div>
-                        </li>
-                        <li class="current-mode">
-                            <div>
-                                <span class="mode-icon"></span>
-                                <span class="mode-desc"><b>{model}购物</b></span>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
                 <div class="lhs-group">
                 </div>
                 <div class="lhs-group" id="ref-category">
                     <p class="ref_active">
                         <span class="rsaquo">›</span>
-                        {allcatigory}所有类别
+                        <?php echo $this->_var['allcat']; ?>
                     </p>
+                    <?php $_from = $this->_var['ocat']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'icat');if (count($_from)):
+    foreach ($_from AS $this->_var['icat']):
+?>
                     <ul>
-                        <li>
-                            <a href="">{catigory}光学产品/光学仪器</a>
-                        </li>
-                        <li class="ref-cat-indent">
-                            <a href="">{subcatigory}望远镜</a>
-                        </li>
+                        <li><a href="<?php echo $this->_var['icat']['catlink']; ?>"><?php echo $this->_var['icat']['catname']; ?></a></li>
+                        <li class="ref-cat-indent"><a href="<?php echo $this->_var['icat']['subcatlink']; ?>"><?php echo $this->_var['icat']['subcatname']; ?></a> </li>
                     </ul>
+                    <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>
+                    
                 </div>
                 <div class="lhs-group" id="ref-price">
                     <p class="ref_active">
                         <span class="rsaquo">›</span>
-                        {allprice}所有价格
+                        <?php echo $this->_var['allprice']; ?>
                     </p>
                     <ul>
+                    	<?php $_from = $this->_var['oprice']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'iprice');if (count($_from)):
+    foreach ($_from AS $this->_var['iprice']):
+?>
+                    	
                         <li>
-                            <a href="" id="price_sugg0">{price} 低于 ￥70</a>
+                            <a href="<?php echo $this->_var['iprice']['pricelink']; ?>" id="price_sugg3"><?php echo $this->_var['iprice']['price']; ?> </a>
                         </li>
-                        <li>
-                            <a href="" id="price_sugg1">{price}￥70 - ￥300</a>
-                        </li>
-                        <li>
-                            <a href="" id="price_sugg4">{price}高于 ￥5,000</a>
-                        </li>
+                        <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>    
                         <li>
                             <form method="get" name="pf" action="/products">
-                                <input name="q" value="望远镜" type="hidden"><input name="output" value="nojs" type="hidden"><input name="hl" value="zh-cn" type="hidden"><input name="lnk" value="pruser" type="hidden"><input gtbfieldid="21" size="5" name="price1" class="price-range-input" type="text">{fromprice}元到
+                                <input name="q" value="<?php echo $this->_var['sekw']; ?>" type="hidden"><input name="output" value="nojs" type="hidden">
+                                <input name="hl" value="zh-cn" type="hidden"><input name="lnk" value="pruser" type="hidden">
+                                <input gtbfieldid="21" size="5" name="price1" class="price-range-input" type="text"><?php echo $this->_var['fromprice']; ?>
                                 <br>
-                                <input gtbfieldid="22" size="5" name="price2" class="price-range-input" type="text">{toprice}元<input value="搜" type="submit">
+                                <input gtbfieldid="22" size="5" name="price2" class="price-range-input" type="text"><?php echo $this->_var['toprice']; ?>
+                                <input value="<?php echo $this->_var['searchbutton']; ?>" type="submit">
                             </form>
                         </li>
                     </ul>
@@ -58,21 +45,16 @@
                 <div class="lhs-group" id="ref-brand">
                     <p class="ref_active">
                         <span class="rsaquo">›</span>
-                        {allbrand}所有品牌
+                        <?php echo $this->_var['allbrand']; ?>
                     </p>
                     <ul>
+                    	<?php $_from = $this->_var['obrand']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'ibrand');if (count($_from)):
+    foreach ($_from AS $this->_var['ibrand']):
+?>
                         <li>
-                            <a href="" id="brand_sugg0" title="nikon">{brand}Nikon</a>
+                            <a href="<?php echo $this->_var['ibrand']['brandlink']; ?>" id="brand_sugg0" title="nikon"><?php echo $this->_var['ibrand']['brand']; ?></a>
                         </li>
-                        <li>
-                            <a href="" id="brand_sugg1">{brand}利康</a>
-                        </li>
-                        <li>
-                            <a href="" id="brand_sugg2" title="{brand}博 冠">{brand}博冠</a>
-                        </li>
-                        <li>
-                            <a href="" id="brand_sugg3" title="{brand}博士 能">{brand}博士能</a>
-                        </li>
+                        <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>
                     </ul>
                 </div>
             </div>
--- php/testproductsmarty.php
+++ php/testproductsmarty.php
@@ -41,111 +41,40 @@
      $smarty->assign('home', 'index.php');   
      $smarty->assign('searchlink', 'index.php');   
      $smarty->assign('searchbox', '搜索商品');     
-     $smarty->assign('searchtitle', '点点淘搜索');   
+     $smarty->assign('searchtitle', '点点淘搜索');
+     $smarty->assign('sekw', '望远镜');
+     $smarty->assign('fromprice', '元到');
+     $smarty->assign('toprice', '元');
+     $smarty->assign('searchbutton','搜');
     #####recent view object
-	$oview = array();
-	while($i++<10){
-		$oview[$i]['img']='images/_uu-1DaNOyy8wgsaXarxI5NJTWV0XOR1axo-TxkQ0FEyFXZ1ir3bnAbddY6U.jpg';
-		$oview[$i]['price']='340$';
-		$oview[$i]['star1']='1';
-		$oview[$i]['star2']='1';
-		$oview[$i]['star3']='1';
-		$oview[$i]['star4']='1';
-		$oview[$i]['star5']='0';
-		$oview[$i]['name']='Samsung2 i9000 ';
-		$oview[$i]['description']='8GB...';
-		$oview[$i]['num']=$i-1;
+	$ocat = array();
+	while($i++<3){
+		$ocat[$i]['catname']='光学产品/光学仪器';
+		$ocat[$i]['catlink']='a.html';
+		$ocat[$i]['subcatname']='望远镜';
+		$ocat[$i]['subcatlink']='a.html';
 	}
-	/***
-	$oview[1]['img']='images/_uu-1DaNOyy8wgsaXarxI5NJTWV0XOR1axo-TxkQ0FEyFXZ1ir3bnAbddY6U.jpg';
-	$oview[1]['price']='340$';
-	$oview[1]['star1']='1';
-	$oview[1]['star2']='1';
-	$oview[1]['star3']='1';
-	$oview[1]['star4']='1';
-	$oview[1]['star5']='0';
-	$oview[1]['name']='Samsung2 i9000 ';
-	$oview[1]['description']='8GB...';
-	$oview[1]['num']='1';
-	
-	$oview[2]['img']='images/_uu-1DaNOyy8wgsaXarxI5NJTWV0XOR1axo-TxkQ0FEyFXZ1ir3bnAbddY6U.jpg';
-	$oview[2]['price']='340$';
-	$oview[2]['star1']='1';
-	$oview[2]['star2']='1';
-	$oview[2]['star3']='1';
-	$oview[2]['star4']='1';
-	$oview[2]['star5']='0';
-	$oview[2]['name']='Samsung2 i9000 ';
-	$oview[2]['description']='8GB...';
-	$oview[2]['num']='2';
-	
-	$oview[3]['img']='images/_uu-1DaNOyy8wgsaXarxI5NJTWV0XOR1axo-TxkQ0FEyFXZ1ir3bnAbddY6U.jpg';
-	$oview[3]['price']='340$';
-	$oview[3]['star1']='1';
-	$oview[3]['star2']='1';
-	$oview[3]['star3']='1';
-	$oview[3]['star4']='1';
-	$oview[3]['star5']='0';
-	$oview[3]['name']='Samsung2 i9000 ';
-	$oview[3]['description']='8GB...';
-	$oview[3]['num']='3';
 
-	$oview[4]['img']='images/_uu-1DaNOyy8wgsaXarxI5NJTWV0XOR1axo-TxkQ0FEyFXZ1ir3bnAbddY6U.jpg';
-	$oview[4]['price']='340$';
-	$oview[4]['star1']='1';
-	$oview[4]['star2']='1';
-	$oview[4]['star3']='1';
-	$oview[4]['star4']='1';
-	$oview[4]['star5']='0';
-	$oview[4]['name']='Samsung2 i9000 ';
-	$oview[4]['description']='8GB...';
-	$oview[4]['num']='4';
-	
-
-
-	$oview[5]['img']='images/_uu-1DaNOyy8wgsaXarxI5NJTWV0XOR1axo-TxkQ0FEyFXZ1ir3bnAbddY6U.jpg';
-	$oview[5]['price']='340$';
-	$oview[5]['star1']='1';
-	$oview[5]['star2']='1';
-	$oview[5]['star3']='1';
-	$oview[5]['star4']='1';
-	$oview[5]['star5']='0';
-	$oview[5]['name']='Samsung2 i9000 ';
-	$oview[5]['description']='8GB...';
-	$oview[5]['num']='4';
-	**/
-	####hot
-	$ohot = array();
-	$ohot[0]['img']='images/_uu-1DaNOyy8wgsaXarxI5NJTWV0XOR1axo-TxkQ0FEyFXZ1ir3bnAbddY6U.jpg';
-	$ohot[0]['price']='340$';
-	$ohot[0]['star1']='1';
-	$ohot[0]['star2']='1';
-	$ohot[0]['star3']='1';
-	$ohot[0]['star4']='1';
-	$ohot[0]['star5']='0';
-	$ohot[0]['name']='Samsung2 i9000 ';
-	$ohot[0]['description']='8GB...';
-	
-	$ohot[1]['img']='images/_uu-1DaNOyy8wgsaXarxI5NJTWV0XOR1axo-TxkQ0FEyFXZ1ir3bnAbddY6U.jpg';
-	$ohot[1]['price']='340$';
-	$ohot[1]['star1']='1';
-	$ohot[1]['star2']='1';
-	$ohot[1]['star3']='1';
-	$ohot[1]['star4']='1';
-	$ohot[1]['star5']='0';
-	$ohot[1]['name']='Samsung2 i9000 ';
-	$ohot[1]['description']='8GB...';
-
-	$okw=array();
+	$oprice=array();
 	$i=0;
-	while($i++<10){
-		$okw[$i][keyword]='手机';
-		$okw[$i][link]='a.htm';
+	while($i++<5){
+		$oprice[$i][price]='低于 70';
+		$oprice[$i][pricelink]='a.htm';
+	}
+	$obrand=array();
+	$i=0;
+	while($i++<5){
+		$obrand[$i][brand]='品牌'.$i;
+		$obrand[$i][brandlink]='a.htm';
 	}
 	
-	$smarty->assign('ohot', $ohot);
-	$smarty->assign('oview', $oview);
-	$smarty->assign('okw', $okw);
+	
+	$smarty->assign('oprice', $oprice);
+	$smarty->assign('ocat', $ocat);
+	$smarty->assign('obrand', $obrand);
+	$smarty->assign('allcat','所有类别');
+	$smarty->assign('allprice', '所有价格');
+	$smarty->assign('allbrand','所有品牌');
 	#####foot object
 	
 		$ofoot = array();
